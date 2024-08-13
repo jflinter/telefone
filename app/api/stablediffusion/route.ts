58073036219c4a9202f3b983abb6dd5ea8f96156
Jack Flintermann <jflinter11@gmail.com>
2024-08-13 15:05:53 +0200

    wip

--- app/api/stablediffusion/route.ts
+++ app/api/stablediffusion/route.ts
@@ -1,4 +1,4 @@
-import { Substrate, StableDiffusionXLLightning } from "substrate";
+import { Substrate, GenerateImage } from "substrate";
 import { saveImage } from "../saveImage";
 
 export const runtime = "edge";
@@ -6,15 +6,12 @@ export const runtime = "edge";
 const substrate = new Substrate({ apiKey: process.env.SUBSTRATE_API_KEY });
 
 async function generateImage(prompt: string): Promise<string> {
-  const image = new StableDiffusionXLLightning({
+  const image = new GenerateImage({
     prompt: prompt,
     store: "hosted",
-    height: 1024,
-    width: 1024,
-    num_images: 1,
   });
   const result = await substrate.run(image);
-  return result.get(image).outputs[0].image_uri;
+  return result.get(image).image_uri;
 }
 
 export async function POST(req: Request) {
